5d9a27c391831a781af56bab7ab0c06fe0c1cd6c
Aravind <gmarav005@gmail.com>
2025-12-12 13:51:31 +0530

    Add Footer component

--- index.jsx
+++ index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home.jsx"
-import About from "./pages/About.jsx"
-import Vans from "./pages/Vans.jsx"
-import VanDetail from "./pages/VanDetail.jsx"
-import Layout from "./components/Layout.jsx"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
 
 import "./server"
 
@@ -13,11 +17,17 @@ export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
