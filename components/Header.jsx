1dd3fe9f28872f5977c5733d6ba745e37150c4e9
Aravind <gmarav005@gmail.com>
2025-12-12 16:53:25 +0530

    Setup the HostVans & HostVanDetail

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
     
     return (
         <header>
@@ -9,19 +14,19 @@ export default function Header() {
             <nav>
                 <NavLink 
                     to="/host"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
                     to="/about"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
                     to="/vans"
-                    className={({isActive}) => isActive ? "active-link" : null}
+                    style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
