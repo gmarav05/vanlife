ea1238cf4ca091c0bdbc5d36ee0766ea060c59ab
Aravind <gmarav005@gmail.com>
2025-12-12 12:13:48 +0530

    Add VanDetail component

--- index.jsx
+++ index.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from "./pages/Vans"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Vans from "./pages/Vans.jsx"
+import VanDetail from "./pages/VanDetail.jsx"
+import Layout from "./components/Layout.jsx"
 
 import "./server"
-
 
 export function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-           <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
